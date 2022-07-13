94bf48799c45e3af9ee2b233e09b51117bbb1b96
Elijah Larsen <elijah@skill-trek.com>
2022-07-12 22:43:42 -0700

    this junk doesn't quite work

--- js/cell.js
+++ js/cell.js
@@ -1,3 +1,4 @@
+
 class Cell {
 
     constructor(x,y) {
@@ -5,11 +6,20 @@ class Cell {
       this.y = y;
 
       this.cellSize = 10;
-      this.margin = 20;
-      this.spacing = 5;
+      this.margin = 20.5;
+      this.spacing = 0;
 
+      this.wallWidth = 1;
+      // this.walls = [this.randomBool(), this.randomBool(), this.randomBool(), this.randomBool()];
+      this.walls = [true,true,true,true];
+    }
 
-      this.walls = [true, true, true, true];
+    breakWall(direction){
+      this.walls[this.getWall(direction)] = false;
+    }
+
+    randomBool(){
+      return Math.random() < 0.5;
     }
 
     getWall(direction){
@@ -24,7 +34,7 @@ class Cell {
                 return 2
                 break;
             case "W":
-                return 0
+                return 3
                 break;
         
             default:
@@ -33,10 +43,60 @@ class Cell {
     }
 
     draw(){
-        square(
-            this.x * (this.cellSize + this.spacing) + this.margin, // y
-            this.y * (this.cellSize + this.spacing) + this.margin, // x
-            this.cellSize // radius
+      // stroke(14);
+      // strokeWeight(1);  
+      // square(
+      //       this.x * (this.cellSize + this.spacing) + this.margin, // x
+      //       this.y * (this.cellSize + this.spacing) + this.margin, // y
+      //       this.cellSize // radius
+      //       );
+        
+        // NORTH WALL
+        if (this.walls[this.getWall("N")]){
+          stroke(255, 0, 0);
+          strokeWeight(this.wallWidth);
+          line(
+            this.x * (this.cellSize + this.spacing) + this.margin, // x1
+            this.y * (this.cellSize + this.spacing) + this.margin, // y1
+            this.x * (this.cellSize + this.spacing) + this.margin + this.cellSize, // x2
+            this.y * (this.cellSize + this.spacing) + this.margin  // y2
             );
+        }
+        
+        // SOUTH WALL
+        if (this.walls[this.getWall("S")]){
+          stroke(0, 0, 255);
+          strokeWeight(this.wallWidth);
+          line(
+            this.x * (this.cellSize + this.spacing) + this.margin, // x1
+            this.y * (this.cellSize + this.spacing) + this.margin + this.cellSize, // y1
+            this.x * (this.cellSize + this.spacing) + this.margin + this.cellSize, // x2
+            this.y * (this.cellSize + this.spacing) + this.margin + this.cellSize  // y2
+            );
+        }
+
+        // EAST WALL
+        if (this.walls[this.getWall("E")]){
+          stroke(0, 255, 0);
+          strokeWeight(this.wallWidth);
+          line(
+            this.x * (this.cellSize + this.spacing) + this.margin + this.cellSize, // x1
+            this.y * (this.cellSize + this.spacing) + this.margin, // y1
+            this.x * (this.cellSize + this.spacing) + this.margin + this.cellSize, // x2
+            this.y * (this.cellSize + this.spacing) + this.margin + this.cellSize  // y2
+            );
+        }
+        
+        // WEST WALL
+        if (this.walls[this.getWall("W")]){
+          stroke(255, 0, 255);
+          strokeWeight(this.wallWidth);
+          line(
+            this.x * (this.cellSize + this.spacing) + this.margin, // x1
+            this.y * (this.cellSize + this.spacing) + this.margin, // y1
+            this.x * (this.cellSize + this.spacing) + this.margin, // x2
+            this.y * (this.cellSize + this.spacing) + this.margin + this.cellSize  // y2
+            );
+        }
     }
   }
--- js/game.js
+++ js/game.js
@@ -1,26 +1,182 @@
 // This is the game file
 
-let cells = [
-  new Cell(0,0),
-  new Cell(0,1),
-  new Cell(1,0),
-  new Cell(1,1)
-];
+let cells = [];
+let totalRows = 3;
+let totalCols = 4;
+
+const connectedCellSets = [];
+
+function union(setA, setB) {
+  const _union = new Set(setA);
+  for (const elem of setB) {
+    _union.add(elem);
+  }
+  return _union;
+}
+
+function connectCells(x,y,direction){
+  let newPair = new Set();
+  newPair.add(x + "," + y);
+  switch (direction) {
+    case "N":
+        if (y > 0)
+        newPair.add(x+ "," +y-1);
+        break;
+    case "E":
+        if (x < totalCols - 1)
+        newPair.add(x+1+ "," +y);;
+        break;
+    case "S":
+        if (y < totalRows-1)
+        newPair.add(x,y+ "," +1);
+        break;
+    case "W":
+        if (x > 0)
+        newPair.add(x-1+ "," +y);
+        break;
+
+    default:
+        break;
+  }
+
+  // look for either of these in an existing set
+  var found = false;
+
+  var unmatchedSets = [];
+  var matchedSet = new Set();
+
+  newPair.forEach(item => {
+    console.log(["looking for ", item])
+    connectedCellSets.forEach(otherSet => {
+      console.log(["checking ", otherSet])
+      if (otherSet.has(item)){
+        newPair.forEach(item => { matchedSet.add(item)});
+        otherSet.forEach(item => { matchedSet.add(item)});
+        // maybe better to have function to get index of which set it's found in
+      }
+    })
+  })
+
+  if (!found){
+    connectedCellSets.push(newPair);
+  }
+
+  console.log(connectedCellSets);
+}
+
 
 function setup() {
-  createCanvas(400, 400);
-  background(80);
+
+  frameRate(2);
+
+  // create cells
+
+
+  createCanvas(totalRows * 150, totalCols * 150);  
+
+  for (const col of Array(totalCols).keys()) {
+    let theColumn = [];
+    for (const row of Array(totalRows).keys()) {
+      theColumn.push(new Cell(col,row));
+    }
+    cells.push(theColumn);
+    //console.log(["we made a column", theColumn]);
+  }
+
+  // make them all connect
+  // START
+  this.breakTheRightWalls(0,0,"W");
+  // FINISH
+  this.breakTheRightWalls(totalCols-1,totalRows-1,"S");
+
+  for (const col of Array(1000).keys()) {
+    //breakRandomWalls();
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function breakRandomWalls(){
+  let x = this.getRandomInt(totalCols);
+  let y = this.getRandomInt(totalRows);
+  let direction = this.randomItem(["N","S","E","W"]);
+  
+
+  
+  // don't break the edges
+  switch (direction) {
+    case "N":
+        if (y > 0)
+        this.breakTheRightWalls(x,y,direction);
+        break;
+    case "E":
+        if (x < totalCols - 1)
+        this.breakTheRightWalls(x,y,direction);
+        break;
+    case "S":
+        if (y < totalRows-1)
+        this.breakTheRightWalls(x,y,direction);
+        break;
+    case "W":
+        if (x > 0)
+        this.breakTheRightWalls(x,y,direction);
+        break;
+
+    default:
+        break;
+  }
+  
+}
+
+function randomItem(items)
+{
+  
+  return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+function breakTheRightWalls(x,y,direction){
+
+  console.log([x,y,direction]);
+  
+  // break the one you targeted
+  cells[x][y].breakWall(direction);
+
+  // now break the matching wall
+  switch (direction) {
+    case "N":
+        if (y > 0)
+          cells[x][y-1].breakWall("S");
+        break;
+    case "E":
+        if (x < totalCols - 1)
+          cells[x+1][y].breakWall("W");
+        break;
+    case "S":
+        if (y < totalRows-1)
+          cells[x][y+1].breakWall("N");
+        break;
+    case "W":
+        if (x > 0)
+          cells[x-1][y].breakWall("E");
+        break;
+
+    default:
+        break;
+  }
 }
 
 function draw() {
-  //if (mouseIsPressed) {
-  //  fill(80);
-  //} else {
-    //fill(255);
-  //}
-  //ellipse(mouseX, mouseY, 80, 80);
 
-  cells.forEach(cell => {
-    cell.draw();
+  clear();
+  background(80);
+
+
+  cells.forEach(col => {
+    col.forEach(cell => {
+      cell.draw();
+    })
   });
 }
